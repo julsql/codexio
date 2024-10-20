40e9233a7c185094b93b325c1741bbf64165f4f7
Debian <debian@vps-3348bddd.vps.ovh.ca>
2024-10-20 10:28:31 +0000

    guincorn requirements

--- bd/bd/wsgi.py
+++ bd/bd/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+# Add the project base directory to the Python path
+sys.path.append('/home/debian/websites/bd_website/bd')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bd.settings')
 
