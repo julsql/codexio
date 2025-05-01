julsql/bd_website

fix(database): Nettoyage données (typage)

bd/main/migrations/0001_initial.py
@@ -23,16 +23,14 @@ class Migration(migrations.Migration):
                 ('illustrator', models.TextField()),
                 ('colorist', models.TextField()),
                 ('publisher', models.TextField()),
-                ('publication_date', models.TextField()),
+                ('publication_date', models.DateField(null=True)),
                 ('edition', models.TextField()),
-                ('number_of_pages', models.TextField()),
-                ('rating', models.TextField()),
-                ('purchase_price', models.TextField()),
-                ('year_of_purchase', models.TextField()),
+                ('number_of_pages', models.IntegerField(null=True)),
+                ('rating', models.TextField(null=True)),
+                ('purchase_price', models.FloatField(null=True)),
+                ('year_of_purchase', models.IntegerField(null=True)),
                 ('place_of_purchase', models.TextField()),
-                ('deluxe_edition', models.TextField()),
-                ('signed_copy', models.TextField()),
-                ('ex_libris', models.TextField()),
+                ('deluxe_edition', models.BooleanField()),
                 ('synopsis', models.TextField()),
                 ('image', models.TextField()),
             ],

julsql/codexio

bd/main/migrations/0001_initial.py
@@ -23,16 +23,14 @@ class Migration(migrations.Migration):
                 ('illustrator', models.TextField()),
                 ('colorist', models.TextField()),
                 ('publisher', models.TextField()),
-                ('publication_date', models.TextField()),
+                ('publication_date', models.DateField(null=True)),
                 ('edition', models.TextField()),
-                ('number_of_pages', models.TextField()),
-                ('rating', models.TextField()),
-                ('purchase_price', models.TextField()),
-                ('year_of_purchase', models.TextField()),
+                ('number_of_pages', models.IntegerField(null=True)),
+                ('rating', models.FloatField(null=True)),
+                ('purchase_price', models.FloatField(null=True)),
+                ('year_of_purchase', models.IntegerField(null=True)),
                 ('place_of_purchase', models.TextField()),
-                ('deluxe_edition', models.TextField()),
-                ('signed_copy', models.TextField()),
-                ('ex_libris', models.TextField()),
+                ('deluxe_edition', models.BooleanField()),
                 ('synopsis', models.TextField()),
                 ('image', models.TextField()),
             ],