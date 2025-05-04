0c35185b9e9ad034cbe940c3d2b7a26b806e910c
Jul SQL <julsql1@gmail.com>
2025-05-04 02:09:41 +0200

    test: désactiver tous les tests bd fugue

--- bd/tests/test_add_album/test_bdfugue_repository.py
+++ bd/tests/test_add_album/test_bdfugue_repository.py
@@ -5,7 +5,7 @@ from main.core.add_album.internal.bdfugue_connexion import BdFugueRepository
 from tests.test_add_album.album_large_data_set import ASTERIX_ISBN, ASTERIX_URLS, ASTERIX_DATA, SAMBRE_ISBN, \
     SAMBRE_DATA, THORGAL_ISBN, THORGAL_DATA, SAULE_ISBN, SAULE_DATA
 
-
+@unittest.skip("CI cannot access Cloudscraper")
 class TestBdFugueRepository(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
@@ -19,22 +19,18 @@ class TestBdFugueRepository(unittest.TestCase):
         with self.assertRaises(AddAlbumError):
             self.bd_repository.get_infos(0)
 
-    @unittest.skip("CI cannot access Cloudscraper")
     def test_get_correct_infos_from_isbn(self) -> None:
         infos = self.bd_repository.get_infos(ASTERIX_ISBN)
         self.assertEqual(ASTERIX_DATA['BDFUGUE'], infos)
 
-    @unittest.skip("CI cannot access Cloudscraper")
     def test_get_correct_infos_from_sambre_isbn(self) -> None:
         infos = self.bd_repository.get_infos(SAMBRE_ISBN)
         self.assertEqual(SAMBRE_DATA['BDFUGUE'], infos)
 
-    @unittest.skip("CI cannot access Cloudscraper")
     def test_get_correct_infos_from_thorgal_isbn(self) -> None:
         infos = self.bd_repository.get_infos(THORGAL_ISBN)
         self.assertEqual(THORGAL_DATA['BDFUGUE'], infos)
 
-    @unittest.skip("CI cannot access Cloudscraper")
     def test_get_correct_infos_from_saule_isbn(self) -> None:
         infos = self.bd_repository.get_infos(SAULE_ISBN)
         self.assertEqual(SAULE_DATA['BDFUGUE'], infos)
