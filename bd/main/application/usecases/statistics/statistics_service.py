julsql/bd_website

hexa(statistics): Réécriture du module Statistics en architecture hexagonale

bd/main/application/usecases/statistics/statistics_service.py
@@ -1,0 +1,23 @@
+from main.domain.model.statistics import Statistics
+from main.domain.ports.repositories.attachment_repository import AttachmentRepository
+from main.domain.ports.repositories.statistics_repository import StatisticsRepository
+
+
+class StatisticsService:
+    def __init__(self, database_repository: StatisticsRepository,
+                 attachment_repository: AttachmentRepository):
+        self._database_repository = database_repository
+        self._attachment_repository = attachment_repository
+
+    def execute(self) -> Statistics:
+        db_stats = self._database_repository.get_database_statistics()
+        attachment_stats = self._attachment_repository.get_attachment_statistics()
+
+        return Statistics(
+            nombre_albums=db_stats.nombre_albums,
+            nombre_pages=db_stats.nombre_pages,
+            prix_total=db_stats.prix_total,
+            nombre_editions_speciales=db_stats.nombre_editions_speciales,
+            nombre_dedicaces=attachment_stats.nombre_dedicaces,
+            nombre_exlibris=attachment_stats.nombre_exlibris
+        )

bd/main/infrastructure/views/statistics_view.py
@@ -1,0 +1,27 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from main.application.usecases.statistics.statistics_service import StatisticsService
+from main.infrastructure.persistence.database.statistics_attachment_adapter import StatisticsAttachmentAdapter
+from main.infrastructure.persistence.database.statistics_database_adapter import StatisticsDatabaseAdapter
+
+
+def statistics_view(request: HttpRequest) -> HttpResponse:
+    database_repository = StatisticsDatabaseAdapter()
+    attachment_repository = StatisticsAttachmentAdapter()
+
+    service = StatisticsService(
+        database_repository=database_repository,
+        attachment_repository=attachment_repository
+    )
+
+    statistics = service.execute()
+
+    return render(request, 'statistics/module.html', {
+        'nombre': statistics.nombre_albums,
+        'pages': statistics.nombre_pages,
+        'prix': statistics.prix_total,
+        'tirage': statistics.nombre_editions_speciales,
+        'dedicaces': statistics.nombre_dedicaces,
+        'exlibris': statistics.nombre_exlibris
+    })

bd/tests/test_statistics/internal/statistics_attachments_in_memory.py
@@ -1,13 +1,14 @@
 from abc import ABC
 
-from main.core.statistics.statistics_attachments_repository import StatisticsAttachmentsRepository
+from main.domain.model.statistics import Statistics
+from main.domain.ports.repositories.attachment_repository import AttachmentRepository
 
 
-class StatisticsAttachmentsInMemory(StatisticsAttachmentsRepository, ABC):
-    def __init__(self, return_value: dict[str, int]) -> None:
+class StatisticsAttachmentsInMemory(AttachmentRepository, ABC):
+    def __init__(self, return_value: Statistics) -> None:
         self.return_value = return_value
         self.get_information_called = False
 
-    def get_information(self) -> dict[str, int]:
+    def get_attachment_statistics(self) -> Statistics:
         self.get_information_called = True
         return self.return_value

bd/tests/test_statistics/internal/statistics_database_in_memory.py
@@ -1,13 +1,14 @@
 from abc import ABC
 
-from main.core.statistics.statistics_database_repository import StatisticsDatabaseRepository
+from main.domain.model.statistics import Statistics
+from main.domain.ports.repositories.statistics_repository import StatisticsRepository
 
 
-class StatisticsDatabaseInMemory(StatisticsDatabaseRepository, ABC):
-    def __init__(self, return_value: dict[str, int]) -> None:
+class StatisticsDatabaseInMemory(StatisticsRepository, ABC):
+    def __init__(self, return_value: Statistics) -> None:
         self.return_value = return_value
         self.get_information_called = False
 
-    def get_information(self) -> dict[str, int]:
+    def get_database_statistics(self) -> Statistics:
         self.get_information_called = True
         return self.return_value

bd/tests/test_statistics/test_statistics_attachments_repository.py
@@ -6,12 +6,12 @@ import unittest
 import django
 
 from main.core.common.data.data import SIGNED_COPY_PATH, EXLIBRIS_PATH
+from main.domain.model.statistics import Statistics
+from main.infrastructure.persistence.database.statistics_attachment_adapter import StatisticsAttachmentAdapter
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
-
-from main.core.statistics.internal.statistics_attachments_connexion import StatisticsAttachmentsConnexion
 
 
 class TestStatisticsAttachmentsConnexion(unittest.TestCase):
@@ -29,7 +29,7 @@ class TestStatisticsAttachmentsConnexion(unittest.TestCase):
         os.makedirs(cls.SIGNED_COPY_FOLDER, exist_ok=True)
         os.makedirs(cls.EXLIBRIS_FOLDER, exist_ok=True)
 
-        cls.repository = StatisticsAttachmentsConnexion(cls.SIGNED_COPY_FOLDER, cls.EXLIBRIS_FOLDER)
+        cls.repository = StatisticsAttachmentAdapter(cls.SIGNED_COPY_FOLDER, cls.EXLIBRIS_FOLDER)
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -55,11 +55,12 @@ class TestStatisticsAttachmentsConnexion(unittest.TestCase):
 
     def test_get_information_empty_database(self) -> None:
         # Act
-        result = self.repository.get_information()
+        result = self.repository.get_attachment_statistics()
 
         # Assert
-        self.assertIsInstance(result, dict)
-        self.assertEqual({'dedicaces': 0, 'exlibris': 0}, result)
+        self.assertIsInstance(result, Statistics)
+        expected = Statistics.empty()
+        self.assertEqual(expected, result)
 
     def test_get_information_with_data(self) -> None:
         # Arrange
@@ -70,26 +71,24 @@ class TestStatisticsAttachmentsConnexion(unittest.TestCase):
         self.create_test_files(self.EXLIBRIS_FOLDER, "987654321", ["1.jpeg", "2.jpeg"])
 
         # Act
-        result = self.repository.get_information()
+        result = self.repository.get_attachment_statistics()
 
         # Assert
-        self.assertEqual({
-            'dedicaces': 5,  # 2 + 3
-            'exlibris': 3,  # 1 + 2
-        }, result)
+        expected = Statistics.empty()
+        expected.nombre_dedicaces = 5  # 2 + 3
+        expected.nombre_exlibris = 3  # 1 + 2
+        self.assertEqual(expected, result)
 
     def test_get_information_with_zero_attachments(self) -> None:
         # Arrange
         self.create_test_files(self.SIGNED_COPY_FOLDER, "123456789", [])
 
         # Act
-        result = self.repository.get_information()
+        result = self.repository.get_attachment_statistics()
 
         # Assert
-        self.assertEqual({
-            'dedicaces': 0,
-            'exlibris': 0
-        }, result)
+        expected = Statistics.empty()
+        self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':

julsql/codexio

bd/main/application/usecases/statistics/statistics_service.py
@@ -1,0 +1,23 @@
+from main.domain.model.statistics import Statistics
+from main.domain.ports.repositories.statistics_attachment_repository import StatisticsAttachmentRepository
+from main.domain.ports.repositories.statistics_database_repository import StatisticsDatabaseRepository
+
+
+class StatisticsService:
+    def __init__(self, database_repository: StatisticsDatabaseRepository,
+                 attachment_repository: StatisticsAttachmentRepository):
+        self._database_repository = database_repository
+        self._attachment_repository = attachment_repository
+
+    def execute(self) -> Statistics:
+        db_stats = self._database_repository.get_database_statistics()
+        attachment_stats = self._attachment_repository.get_attachment_statistics()
+
+        return Statistics(
+            nombre_albums=db_stats.nombre_albums,
+            nombre_pages=db_stats.nombre_pages,
+            prix_total=db_stats.prix_total,
+            nombre_editions_speciales=db_stats.nombre_editions_speciales,
+            nombre_dedicaces=attachment_stats.nombre_dedicaces,
+            nombre_exlibris=attachment_stats.nombre_exlibris
+        )

bd/main/infrastructure/views/statistics_view.py
@@ -1,0 +1,27 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from main.application.usecases.statistics.statistics_service import StatisticsService
+from main.infrastructure.persistence.database.statistics_database_adapter import StatisticsDatabaseAdapter
+from main.infrastructure.persistence.file.statistics_attachment_adapter import StatisticsAttachmentAdapter
+
+
+def statistics_view(request: HttpRequest) -> HttpResponse:
+    database_repository = StatisticsDatabaseAdapter()
+    attachment_repository = StatisticsAttachmentAdapter()
+
+    service = StatisticsService(
+        database_repository=database_repository,
+        attachment_repository=attachment_repository
+    )
+
+    statistics = service.execute()
+
+    return render(request, 'statistics/module.html', {
+        'nombre': statistics.nombre_albums,
+        'pages': statistics.nombre_pages,
+        'prix': statistics.prix_total,
+        'tirage': statistics.nombre_editions_speciales,
+        'dedicaces': statistics.nombre_dedicaces,
+        'exlibris': statistics.nombre_exlibris
+    })

bd/tests/test_statistics/internal/statistics_attachments_in_memory.py
@@ -1,13 +1,14 @@
 from abc import ABC
 
-from main.core.statistics.statistics_attachments_repository import StatisticsAttachmentsRepository
+from main.domain.model.statistics import Statistics
+from main.domain.ports.repositories.statistics_attachment_repository import StatisticsAttachmentRepository
 
 
-class StatisticsAttachmentsInMemory(StatisticsAttachmentsRepository, ABC):
-    def __init__(self, return_value: dict[str, int]) -> None:
+class StatisticsAttachmentsInMemory(StatisticsAttachmentRepository, ABC):
+    def __init__(self, return_value: Statistics) -> None:
         self.return_value = return_value
         self.get_information_called = False
 
-    def get_information(self) -> dict[str, int]:
+    def get_attachment_statistics(self) -> Statistics:
         self.get_information_called = True
         return self.return_value

bd/tests/test_statistics/internal/statistics_database_in_memory.py
@@ -1,13 +1,14 @@
 from abc import ABC
 
-from main.core.statistics.statistics_database_repository import StatisticsDatabaseRepository
+from main.domain.model.statistics import Statistics
+from main.domain.ports.repositories.statistics_database_repository import StatisticsDatabaseRepository
 
 
 class StatisticsDatabaseInMemory(StatisticsDatabaseRepository, ABC):
-    def __init__(self, return_value: dict[str, int]) -> None:
+    def __init__(self, return_value: Statistics) -> None:
         self.return_value = return_value
         self.get_information_called = False
 
-    def get_information(self) -> dict[str, int]:
+    def get_database_statistics(self) -> Statistics:
         self.get_information_called = True
         return self.return_value

bd/tests/test_statistics/test_statistics_attachments_repository.py
@@ -6,12 +6,12 @@ import unittest
 import django
 
 from main.core.common.data.data import SIGNED_COPY_PATH, EXLIBRIS_PATH
+from main.domain.model.statistics import Statistics
+from main.infrastructure.persistence.file.statistics_attachment_adapter import StatisticsAttachmentAdapter
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
-
-from main.core.statistics.internal.statistics_attachments_connexion import StatisticsAttachmentsConnexion
 
 
 class TestStatisticsAttachmentsConnexion(unittest.TestCase):
@@ -29,7 +29,7 @@ class TestStatisticsAttachmentsConnexion(unittest.TestCase):
         os.makedirs(cls.SIGNED_COPY_FOLDER, exist_ok=True)
         os.makedirs(cls.EXLIBRIS_FOLDER, exist_ok=True)
 
-        cls.repository = StatisticsAttachmentsConnexion(cls.SIGNED_COPY_FOLDER, cls.EXLIBRIS_FOLDER)
+        cls.repository = StatisticsAttachmentAdapter(cls.SIGNED_COPY_FOLDER, cls.EXLIBRIS_FOLDER)
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -55,11 +55,12 @@ class TestStatisticsAttachmentsConnexion(unittest.TestCase):
 
     def test_get_information_empty_database(self) -> None:
         # Act
-        result = self.repository.get_information()
+        result = self.repository.get_attachment_statistics()
 
         # Assert
-        self.assertIsInstance(result, dict)
-        self.assertEqual({'dedicaces': 0, 'exlibris': 0}, result)
+        self.assertIsInstance(result, Statistics)
+        expected = Statistics.empty()
+        self.assertEqual(expected, result)
 
     def test_get_information_with_data(self) -> None:
         # Arrange
@@ -70,26 +71,24 @@ class TestStatisticsAttachmentsConnexion(unittest.TestCase):
         self.create_test_files(self.EXLIBRIS_FOLDER, "987654321", ["1.jpeg", "2.jpeg"])
 
         # Act
-        result = self.repository.get_information()
+        result = self.repository.get_attachment_statistics()
 
         # Assert
-        self.assertEqual({
-            'dedicaces': 5,  # 2 + 3
-            'exlibris': 3,  # 1 + 2
-        }, result)
+        expected = Statistics.empty()
+        expected.nombre_dedicaces = 5  # 2 + 3
+        expected.nombre_exlibris = 3  # 1 + 2
+        self.assertEqual(expected, result)
 
     def test_get_information_with_zero_attachments(self) -> None:
         # Arrange
         self.create_test_files(self.SIGNED_COPY_FOLDER, "123456789", [])
 
         # Act
-        result = self.repository.get_information()
+        result = self.repository.get_attachment_statistics()
 
         # Assert
-        self.assertEqual({
-            'dedicaces': 0,
-            'exlibris': 0
-        }, result)
+        expected = Statistics.empty()
+        self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':