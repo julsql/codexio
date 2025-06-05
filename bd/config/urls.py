julsql/bd_website

hexa(add-album): Réécriture du module AddAlbum en architecture hexagonale

bd/config/urls.py
@@ -18,7 +18,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from main.core.add_album.internal.add_album_view import add_album
 from main.core.attachments.internal.attachments_view import signed_copies, exlibris
 from main.core.bd_search.internal.bd_search_view import bd_search
 from main.core.delete_photo.internal.delete_photo_view import delete_dedicace, delete_exlibris
@@ -28,6 +27,7 @@ from main.core.home.internal.home_view import home
 from main.core.page_bd.internal.page_bd_view import page_bd
 from main.core.update_database.internal.update_database_view import update_database
 from main.core.upload_photo.internal.upload_photo_view import upload_dedicace, upload_exlibris
+from main.infrastructure.views.album_view import add_album
 from main.infrastructure.views.statistics_view import statistics_view
 
 handler500 = error_500_view

julsql/codexio

bd/config/urls.py
@@ -18,7 +18,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from main.core.add_album.internal.add_album_view import add_album
 from main.core.attachments.internal.attachments_view import signed_copies, exlibris
 from main.core.bd_search.internal.bd_search_view import bd_search
 from main.core.delete_photo.internal.delete_photo_view import delete_dedicace, delete_exlibris
@@ -28,6 +27,7 @@ from main.core.home.internal.home_view import home
 from main.core.page_bd.internal.page_bd_view import page_bd
 from main.core.update_database.internal.update_database_view import update_database
 from main.core.upload_photo.internal.upload_photo_view import upload_dedicace, upload_exlibris
+from main.infrastructure.views.add_album_view import add_album
 from main.infrastructure.views.statistics_view import statistics_view
 
 handler500 = error_500_view