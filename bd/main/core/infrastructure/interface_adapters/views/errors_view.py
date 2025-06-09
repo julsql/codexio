8f2cae86bdfa48a464cc994c7d1747cf819e2fe7
Jul SQL <julsql1@gmail.com>
2025-06-09 23:15:44 +0200

    fix(error): Affiche le message d'erreur

--- bd/main/core/infrastructure/interface_adapters/views/errors_view.py
+++ bd/main/core/infrastructure/interface_adapters/views/errors_view.py
@@ -1,7 +1,8 @@
 from typing import Literal
 
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import render
+from django.urls import Resolver404
 
 from main.core.infrastructure.logging.python_logger_adapter import PythonLoggerAdapter
 
@@ -16,7 +17,16 @@ class ErrorView:
         else:
             self.logger_adapter.error(f"{code} Error")
 
-        return render(request, f'errors/{code}.html', status=code)
+        message = "La page que vous recherchez n'existe pas."
+        if exception and isinstance(exception, Resolver404):
+            pass
+        elif exception and isinstance(exception, Http404):
+            message = str(exception)
+
+        return render(request, f'errors/{code}.html', {
+            'message': message,
+        },
+                      status=code)
 
 
 def error_404_view(request: HttpRequest, exception: Exception) -> HttpResponse:
