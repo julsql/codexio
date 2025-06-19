9671b512ec637befcf3244dd1f3de2f363d5e55c
Jul SQL <julsql1@gmail.com>
2025-06-19 20:54:43 +0200

    test(add-album): Test add album view

---
+++ bd/tests/test_add_album/test_add_album_view.py
@@ -0,0 +1,48 @@
+import os
+import sys
+
+import django
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
+
+from django.test import TestCase, Client
+from django.urls import reverse
+from unittest.mock import patch
+from main.core.domain.model.profile_type import ProfileType
+from main.core.infrastructure.persistence.database.models import Collection, AppUser
+
+
+class AddAlbumIntegrationTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.isbn = 1234567890
+        self.token = 'Bearer test-token'
+        self.headers = {'HTTP_AUTHORIZATION': self.token}
+
+        user = AppUser.objects.get(username="admin")
+        self.collection = Collection.objects.get(accounts=user)
+
+    @patch('main.core.application.usecases.authorization.authorization_service.AuthorizationService.verify_token')
+    @patch(
+        'main.core.infrastructure.interface_adapters.profile_type.profile_type_adapter.ProfileTypeAdapter.get_profile_type')
+    @patch('main.core.application.usecases.add_album.add_album_service.AddAlbumService.main')
+    def test_add_album_success(self, mock_add_album, mock_get_profile_type, mock_verify_token):
+        # Mock les méthodes nécessaires
+        mock_verify_token.return_value = self.collection
+        mock_get_profile_type.return_value = ProfileType.BD
+        mock_add_album.return_value = None  # Pas d'erreur = succès
+
+        # Effectuer une requête GET (ton endpoint utilise GET)
+        response = self.client.get(
+            reverse('add_album', kwargs={'isbn': self.isbn}),
+            **self.headers
+        )
+
+        # Vérifie la réponse
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("ajouté avec succès", response.content.decode())
+
+        # Assure que la logique a été appelée
+        mock_add_album.assert_called_once_with(self.isbn)
