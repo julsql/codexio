059afdf0f2c469fe49e355ef5747f953241ff502
Jul SQL <julsql1@gmail.com>
2025-11-26 22:03:04 +0100

    refacto: rename project

--- src/config/settings.py
+++ src/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '***REMOVED***', 'bd.julsql.fr', 'bd', 'testserver', 'codexio.julsql.fr']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '***REMOVED***', 'testserver', 'codexio.julsql.fr', 'codexio']
 
 # Application definition
 
@@ -121,7 +121,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://bd.julsql.fr',
     'https://codexio.julsql.fr',
 ]
 
