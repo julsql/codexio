49aede1fd6af3699b7d0c9700f2e9d8d870d0046
Jul SQL <julsql1@gmail.com>
2026-04-30 21:30:50 +0200

    fix: test book

--- src/tests/test_add_album/test_book_repository.py
+++ src/tests/test_add_album/test_book_repository.py
@@ -6,7 +6,7 @@ from tests.test_add_album.book_large_data_set import BOVARY_ISBN, BOVARY_DATA
 from tests.test_common.internal.logger_in_memory import LoggerInMemory
 
 
-class TestBdPhileRepository(unittest.TestCase):
+class TestBookRepository(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.logging_repository = LoggerInMemory()
@@ -16,9 +16,20 @@ class TestBdPhileRepository(unittest.TestCase):
         with self.assertRaises(ApiConnexionDataNotFound):
             self.bd_repository.get_infos(0)
 
-    def test_get_correct_infos_from_asterix_isbn(self) -> None:
+    def test_get_correct_infos_from_bovary_isbn(self) -> None:
         infos = self.bd_repository.get_infos(BOVARY_ISBN)
-        self.assertEqual(BOVARY_DATA['BOOK'], infos)
+        expected = BOVARY_DATA['BOOK']
+
+        self.assertRegex(
+            infos.image,
+            r"^http://books\.google\.com/books/content\?id=[^&]+&printsec=frontcover&img=1&zoom=1&source=gbs_api$",
+        )
+
+        actual_without_image = infos.copy()
+        actual_without_image.image = ""
+        expected_without_image = expected.copy()
+        expected_without_image.image = ""
+        self.assertEqual(expected_without_image, actual_without_image)
 
 
 
